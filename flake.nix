208ad031c126bb88247636b9e0237a12f138dc3b
wallago <45556867+wallago@users.noreply.github.com>
2025-05-31 19:10:13 +0200

    fix: nixpkgs lts version

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nix starter config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
